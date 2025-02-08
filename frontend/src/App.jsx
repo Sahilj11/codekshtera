03e0ee31b87ca2a06c096f3b85e62b9b93178fa3
Dinesh <dineshgarig@gmail.com>
2025-02-08 18:19:38 +0530

    Auth Slice and Job Slice is also Complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,19 +5,27 @@ import { Routes  } from "react-router-dom"
 import { Route } from "react-router-dom"
 import Login from "./components/Login"
 import Signup from "./components/Signup"
+import RecuratorDashboard from "./components/RecutorDashboard"
+import UserDashboard from "./components/UserDashboard"
+import { Provider } from "react-redux"
+import { store } from "./store/store"
+import MainDashboard from "./components/MainDashboard"
 function App() {
   return (
     <>
+       <Provider store={store}>
       <div className=' h-screen w-screen overflow-x-hidden'>
-
+        
         <Navbar/>
         <Routes>
-          <Route path="/" element={<Home />} />
+        <Route path="/" element={<><Home /><MainDashboard /></>} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/recurator-dashboard" element={<RecuratorDashboard/>}/>
+          <Route path="/user-dashboard" element={<UserDashboard/>}/>
         </Routes>
-        
       </div>
+      </Provider>
     </>
   )
 }
