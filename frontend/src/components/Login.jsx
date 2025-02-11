4f4797509dbefc4518a8f4f0ebb1d5716bbfdd9a
Dinesh <dineshgarig@gmail.com>
2025-02-11 19:38:47 +0530

    Frontend Folder Issue Resolve

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
 
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100 dark:bg-black">
-      <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
+      <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-5 md:p-8 shadow-input bg-white dark:bg-black">
         <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
           Welcome Back to JobPortal
         </h2>
